8967ba6a418fd4d96bbbed6a55eacee684808400
Raul Alvarez <raul.alvarez@magnet.cl>
2022-03-21 12:15:48 -0300

    feat: add dummy change

--- base/views.py
+++ base/views.py
@@ -5,4 +5,4 @@ from rest_framework.response import Response
 
 class PingApiView(views.APIView):
     def get(self, request, format=None):
-        return Response("pong")
+        return Response("pang")
