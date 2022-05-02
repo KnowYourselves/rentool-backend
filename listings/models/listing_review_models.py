ad26707eb8bf2ac155f8101082ba0be78d632eb4
KnowYourselves <rsalvarez@uc.cl>
2022-05-01 22:49:23 -0400

    feat(models): add listing review model

---
+++ listings/models/listing_review_models.py
@@ -0,0 +1,23 @@
+# django
+from django.core import validators
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Review(models.Model):
+    listing = models.ForeignKey(
+        "listings.ToolListing",
+        verbose_name=_("listing"),
+        related_name="reviews",
+        on_delete=models.CASCADE,
+    )
+    score = models.SmallIntegerField(
+        default=0,
+        validators=[
+            validators.MinValueValidator(1),
+            validators.MaxValueValidator(5),
+        ],
+    )
+    description = models.TextField(
+        verbose_name=_("description"),
+    )
