dbb58f0d63c4b9b39e3b89bff8438ff52c7af13f
Raul Alvarez <raul.alvarez@magnet.cl>
2022-05-03 18:06:09 -0400

    feat(models): add past tool listing, remove rented tool

--- listings/models/__init__.py
+++ listings/models/__init__.py
@@ -1,5 +1,6 @@
+from listings.models.base_listing_models import BaseListing
 from listings.models.listing_review_models import Review
-from listings.models.rented_tool_models import RentedTool
+from listings.models.past_tool_listing_models import PastToolListing
 from listings.models.tool_listing_models import ToolListing
 
-__all__ = ["RentedTool", "Review", "ToolListing"]
+__all__ = ["BaseListing", "PastToolListing", "Review", "ToolListing"]
---
+++ listings/models/past_tool_listing_models.py
@@ -0,0 +1,27 @@
+# django
+from django.conf import settings
+from django.db import models
+from django.forms.models import model_to_dict
+
+from listings.models import BaseListing
+
+
+class PastToolListing(BaseListing):
+    renter = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    listing = models.ForeignKey(
+        "listings.ToolListing",
+        on_delete=models.CASCADE,
+        related_name="past_listings",
+    )
+
+    @classmethod
+    def create_from_listing(cls, listing, renter):
+        return cls.objects.create(
+            **model_to_dict(listing, exclude=("publisher",)),
+            renter=renter,
+            listing=listing,
+        )
