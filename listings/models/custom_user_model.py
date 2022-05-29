10281fd7215c4ae07888bde5e40aa67d00522a80
Nicolas <naquiroz@uc.cl>
2022-05-29 16:52:55 -0400

    feat: add no-blank

--- listings/models/custom_user_model.py
+++ listings/models/custom_user_model.py
@@ -7,5 +7,5 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = ["email", "phone_number"]
     phone_number_regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
     phone_number = models.CharField(
-        validators=[phone_number_regex], max_length=16, unique=True
+        validators=[phone_number_regex], max_length=16, unique=True, blank=False
     )
