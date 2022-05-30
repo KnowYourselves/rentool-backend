38650b14a6e0667cb9135ad42a4244731ab6f5c0
KnowYourselves <rsalvarez@uc.cl>
2022-05-30 02:14:59 -0400

    feat: use current user for tool listing publisher

--- listings/serializers/tool_listing_serializer.py
+++ listings/serializers/tool_listing_serializer.py
@@ -5,6 +5,15 @@ from listings.models import ToolListing
 
 
 class ToolListingSerializer(serializers.ModelSerializer):
+    publisher = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+    )
+
     class Meta:
         model = ToolListing
         fields = "__all__"
+
+    def save(self, **kwargs):
+        kwargs["publisher"] = self.fields["publisher"].get_default()
+        return super().save(**kwargs)
