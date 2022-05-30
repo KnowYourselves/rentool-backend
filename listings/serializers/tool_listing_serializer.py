6fb5e42337bc9edbd72d20949b48517c3dc386e9
Nicolas <naquiroz@uc.cl>
2022-05-30 17:31:40 -0400

    feat: add image

--- listings/serializers/tool_listing_serializer.py
+++ listings/serializers/tool_listing_serializer.py
@@ -4,20 +4,11 @@ from rest_framework import serializers
 from listings.models import ToolListing
 
 
-from drf_extra_fields.fields import Base64ImageField
-
-
-class UploadedBase64ImageSerializer(serializers.Serializer):
-    file = Base64ImageField(required=False)
-
-
 class ToolListingSerializer(serializers.ModelSerializer):
     publisher = serializers.PrimaryKeyRelatedField(
         read_only=True,
         default=serializers.CurrentUserDefault(),
     )
-
-    image = UploadedBase64ImageSerializer()
 
     class Meta:
         model = ToolListing
